b8e10c07c8b9262e60d00eaf5956af72f4d5b401
pwangy <blipsandclicks@gmail.com>
2024-01-11 17:32:08 -0800

    doger passes all tests and works in browser

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+const dodger = document.getElementById('dodger')
+
+dodger.style.backgroundColor = '#ff69b4'
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+	if (left > 0) {
+		dodger.style.left = `${left - 1}px` // Move dodger left only if he isn't already at the leftmost position
+	}
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+	if (left < 360) {
+		dodger.style.left = `${left + 1}px` // Move dodger right only if he isn't already at the rightmost position
+	}
+}
+
+document.addEventListener('keydown', function(e) {
+	if (e.key === 'ArrowLeft') {
+		moveDodgerLeft()
+        // console.log('move left')
+	}
+})
+
+document.addEventListener('keydown', function(e) {
+	if (e.key === 'ArrowRight') {
+        moveDodgerRight()
+        // console.log('move right')
+    }
+})
